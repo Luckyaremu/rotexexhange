0f99a159c09f8af641138a554d8464c852745297
Luckyaremu <aremu.baba70@gmail.com>
2021-10-12 22:02:12 +0100

    initial commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -26,6 +26,7 @@ const Homepage = () => {
       </div>
       <Title level={2} className="heading">Global Cryptocurrency Statistics</Title>
       <Row gutter={[32, 32]}>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
         <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
         <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
