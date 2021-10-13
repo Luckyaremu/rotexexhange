9282ffade576b4bccc9fe1518fa11b0d05e52072
Luckyaremu <aremu.baba70@gmail.com>
2021-10-13 11:53:30 +0100

    initial commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
@@ -26,11 +25,11 @@ const Homepage = () => {
       </div>
       <Title level={2} className="heading">Global Crypto Stats</Title>
       <Row gutter={[32, 32]}>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.totals} /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
-        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={globalStats.totalExchanges} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={globalStats.totalMarketCap} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={globalStats.total24hVolume} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={globalStats.totalMarkets} /></Col>
       </Row>
       <div className="home-heading-container">
         <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
