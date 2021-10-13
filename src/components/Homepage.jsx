83fc6bc220c6a560c05004664db12885ac65ed92
Luckyaremu <aremu.baba70@gmail.com>
2021-10-13 22:49:44 +0100

    initial commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -24,7 +24,7 @@ const Homepage = () => {
         <h6>We buy all kinds of cryptocurrencies for trading</h6>
         <a href="http://www.rhotexexchange.com/" target="_blank" rel="noreferrer">SELL YOUR CRIPTOCURRENCY</a>
       </div>
-      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Title level={2} className="heading">Global Crypto Statistics</Title>
       <Row gutter={[32, 32]}>
         <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
         <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
@@ -33,12 +33,12 @@ const Homepage = () => {
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
       <div className="home-heading-container">
-        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
+        <Title level={2} className="home-title">Top 10 Cryptocurrency In The World</Title>
         <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
       </div>
       <Cryptocurrencies simplified />
       <div className="home-heading-container">
-        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={2} className="home-title">Latest Cryptocurrency News</Title>
         <Title level={3}><Link to="/news">Show more</Link></Title>
       </div>
       <News simplified />
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,15 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
   'x-bingapis-sdk': 'true',
-  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-  'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+  'x-rapidapi-key': '4e9bdb1b52msh83fe649289108e9p1aae2ejsn3c6885de129e',
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
 };
 
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
       query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
