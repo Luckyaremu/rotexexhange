485330f3b7bf08d2111150ddc932663a0262d117
Luckyaremu <aremu.baba70@gmail.com>
2021-10-13 20:38:30 +0100

    initial commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Typography } from 'antd';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
@@ -11,7 +12,8 @@ import '../style/Homepage.css';
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { isFetching } = useGetCryptosQuery(10);
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
 
   if (isFetching) return <Loader />;
 
@@ -22,6 +24,14 @@ const Homepage = () => {
         <h6>We buy all kinds of cryptocurrencies for trading</h6>
         <a href="http://www.rhotexexchange.com/" target="_blank" rel="noreferrer">SELL YOUR CRIPTOCURRENCY</a>
       </div>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Row gutter={[32, 32]}>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+      </Row>
       <div className="home-heading-container">
         <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
         <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
